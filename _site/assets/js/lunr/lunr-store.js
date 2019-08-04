255b185ba738a2c484b763b127c0871128c56dd8
Margaret Lazarovits <margaretlazarovits@Margarets-Air-2.attlocal.net>
2019-08-04 17:00:52 -0500

    added header image to home page

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,29 +3,29 @@ var store = [{
         "excerpt":"In our first meeting of the summer, we enjoyed a lecture from Professor Chris Rogan about statistics in the context of machine learning. Margaret started off the meeting with a brief introduction on the purpose of Learning Machine Learning, what the club did last semester and what we plan to...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/LearningMachineLearning/2019/06/27/first-summer19-meeting.html",
-        "teaser":null},{
+        "teaser":"http://localhost:4000/LearningMachineLearning/assets/images/teaser.jpg"},{
         "title": "7/3/19: Intro to ML and Linear Regression Tutorial",
         "excerpt":"This week, we split our time between learning about the mathematical basis of machine learning with Professor Chris Rogan and applying this basis in a Python tutorial with Dr. Sadia Khalil. Professor Rogan gave a brief review of Bayes’ theorem from last week, reiterating the purposes of the prior and...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/LearningMachineLearning/2019/07/03/intro-to-ml-and-linear-regression-tutorial.html",
-        "teaser":null},{
+        "teaser":"http://localhost:4000/LearningMachineLearning/assets/images/teaser.jpg"},{
         "title": "7/10/19 Paper Discussion: Conditional Neural Processes",
         "excerpt":"This week, Dr. Sadia Khalil and Mitzi Sanchez Cubedo led a discussion surrounding the paper, Conditional Neural Processes by Marta Garnelo, et. al.   Below are the slides presented at this meeting.   This is an embedded Microsoft Office presentation, powered by Office Online.  ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/LearningMachineLearning/2019/07/10/paper-discussion-conditional-neural-processes.html",
-        "teaser":null},{
+        "teaser":"http://localhost:4000/LearningMachineLearning/assets/images/teaser.jpg"},{
         "title": "7/17/19 Paper Discussion: Quantum Machine Learning",
         "excerpt":"We discussed the paper Quantum Machine Learning by Jacob Biamonte, et. al. Ryan and Ty led our discussion, which mainly focused on the feasibility of quantum computation. This paper is a survey of current state of quantum machine learning. It addresses the feasability of quantum machine learning and potential applications...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/LearningMachineLearning/2019/07/17/paper-discussion-quantum-ML.html",
-        "teaser":null},{
+        "teaser":"http://localhost:4000/LearningMachineLearning/assets/images/teaser.jpg"},{
         "title": "7/24/19 Tutorial: Tensorflow",
         "excerpt":"We took a break from our regular paper discussions this week to host a tutorial on Tensorflow, a popular machine learning open-source software library. This is a low-level library that is used for higher level APIs, like Keras. We followed this tutorial (just the Introduction section) to get a feel...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/LearningMachineLearning/2019/07/24/tutorial-tensorflow.html",
-        "teaser":null},{
+        "teaser":"http://localhost:4000/LearningMachineLearning/assets/images/teaser.jpg"},{
         "title": "7/31/19 Paper Discussion: Batch Normalization",
         "excerpt":"This week Micah led us in a discussion this paper on batch normalization (BN), which is a technique used in machine learning to speed up learning accurately. The way batch normalization works is by renormalizing the distribution of the data after it passes through a layer in a neural network....","categories": [],
         "tags": [],
         "url": "http://localhost:4000/LearningMachineLearning/2019/07/31/paper-discussion-batch-normalization.html",
-        "teaser":null}]
+        "teaser":"http://localhost:4000/LearningMachineLearning/assets/images/teaser.jpg"}]
--- index.md
+++ index.md
@@ -11,14 +11,15 @@ feature_row:
   - title: "Spring 2019"
     excerpt: "[Events](schedules) from the past semester."
 
----
-<br>
-<center>
-	<h1>Learning Machine Learning at KU</h1>
-	We are a machine learning club under KU's physics department. We hold weekly meetings during the Fall, Spring and Summer semesters. For more information about what we do, or machine learning in general, click the links above!
-</center>
+excerpt: "We are a machine learning club under KU's physics department. We hold weekly meetings during the Fall, Spring and Summer semesters. For more information about what we do, or machine learning in general, click the links above!"
+header:
+  overlay_image: /assets/images/teaser.jpg
+  overlay_filter: 0.5 # same as adding an opacity of 0.5 to a black background
+  caption: "Photo credit: [Franki Chamaki](https://unsplash.com/@franki?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText) on [**Unsplash**](https://unsplash.com)"
 
-<br>
+
+---
+
 
 ---
 {% include feature_row %}
